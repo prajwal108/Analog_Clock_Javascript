7a85ebc3a5e1ed61afdae73e808f14b442349851
prajwal108 <87636245+prajwal108@users.noreply.github.com>
2023-01-14 18:06:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ let hourHand=document.getElementById('hourHand');
 let minHand = document.getElementById('minHand');
 let secHand = document.getElementById('secHand');
 let audioElement = new Audio(`audio/Clock.mp3`);
+let day = document.getElementById('day');
+let month = document.getElementById('month');
+let myDate = document.getElementById('myDate');
+let days = ["SUN","MON","TUE","WED","THU","FRI","SAT"];
+let months = ["JAN","FEB","MAR","APR","MAY","JUN","JUL","AUG","SEP","OCT","NOV","DEC"];
 
 setInterval(()=>{
 let d= new Date();
@@ -15,4 +20,7 @@ hourHand.style.transform=`rotate(${h_rot}deg)`;
 minHand.style.transform=`rotate(${m_rot}deg)`;
 secHand.style.transform=`rotate(${s_rot}deg)`;
 audioElement.play();
+day.innerHTML= days[d.getDay()];
+month.innerHTML=months[d.getMonth()];
+myDate.innerHTML=d.getDate();
 },1000);
